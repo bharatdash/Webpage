0d5e553cb3e09af35dfe7406050778b830e8b783
slathi <lathi.saumya@gmail.com>
2017-02-17 17:37:20 +0530

    After putting only single function in every it block in customersSpec file

--- Spec/customersSpec.js
+++ Spec/customersSpec.js
@@ -3,34 +3,73 @@ var loginPage = require('../Pages/loginPage.js');
 var customerPage = require('../Pages/customersPage.js');
 //Test for Customer page
 describe("Verification of Customer page" , function(){
-	//Opens the web-site before spec
-	browser.get("http://amasik.com/demo/angularjs/angular-app/#/login");
 
-	//it block to check all 5 attributes of customer are present or not
-	it("Should login ,click on customer tab and verify customer page" , function(){
+	beforeEach (function(){
+
+		//Opens the web-site before every spec
+		browser.get("http://amasik.com/demo/angularjs/angular-app/#/login");
+
 		//login the user
 		loginPage.verifyForValidCredentials();
 		//click on customer tab
 		customerPage.clickOnCustomerTab();
 		customerPage.verifyCustomerPage();
+
 	});
 
 	//It block to sort the records of the customer table
-	it("Should sort all the records" , function(){
+	it("Should sort by first name" , function(){
+
 		customerPage.sortByFname();
+	});
+
+	it("Should sort by last name",function(){
+
 		customerPage.sortByLname();
+	});
+
+	it("Should sort by last name",function(){
+
 		customerPage.sortByEmail();
+	});
+
+	it("Should sort by last name",function(){
+
 		customerPage.sortByPhone();
+	});
+
+	it("Should sort by last name",function(){
+
 		customerPage.sortByCountry();
+
+	});
+
+
+	it("Should verify all the search fields",function(){
+
 		customerPage.searchForCustomer();
+
 	});
 
 	//to verify Number of records per pages should not exceed the max limit and page number of the table
-	it("Number of records per pages should not exceed the max limit " , function(){
+	it("Number of records per pages should not exceed the max limit of 10 " , function(){
+
 		customerPage.maxRecordsTen();
+	});
+
+	it("Number of records per pages should not exceed the max limit of 25 " , function(){
+
 		customerPage.maxRecords25();
+	});
+
+	it("Opens second page" , function(){
+
 		customerPage.pageTwo();
+	});
+
+	it("Opens second page" , function(){	
 		customerPage.pageOne();
+
 	});
 
 	//To verify that view button and view tab are working
--- conf.js
+++ conf.js
@@ -4,7 +4,7 @@ exports.config = {
 		framework: 'jasmine',
 		seleniumAddress: 'http://localhost:4444/wd/hub',
 		
-		specs: ['spec/reportsSpec.js'],
+		specs: ['spec/customersSpec.js'],
 
 		jasmineNodeOpts: {defaultTimeoutInterval: 600000},
 		onPrepare: function(){
