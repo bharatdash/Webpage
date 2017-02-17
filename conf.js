c152e1c4a1dd0fef5743ab4976c87c1062e78b5f
bharatdash <bharatdash8@gmail.com>
2017-02-17 15:08:12 +0530

    Update conf

--- conf.js
+++ conf.js
@@ -3,13 +3,8 @@ exports.config = {
 
 		framework: 'jasmine',
 		seleniumAddress: 'http://localhost:4444/wd/hub',
-<<<<<<< HEAD
-		specs: ['spec/securitySpec.js'],
-=======
+		specs: ['spec/loginSpec.js'],
 
-		specs: ['spec/securitySpec.js'],
-
->>>>>>> 3aa5c858e3ed4c07ddbb969306b6a783764b699b
 		jasmineNodeOpts: {defaultTimeoutInterval: 600000},
 		onPrepare: function(){
 			browser.manage().window().maximize();
