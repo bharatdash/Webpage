09f5a40396892372c3a2e6afdae7b6aed93282f7
slathi <lathi.saumya@gmail.com>
2017-02-20 18:39:43 +0530

    Adding the code in conf.js file to generate report

--- conf.js
+++ conf.js
@@ -1,13 +1,20 @@
+var Jasmine2HtmlReporter = require('protractor-jasmine2-html-reporter');
+
 exports.config = {
 		directConnect:true,
 
 		framework: 'jasmine',
 		seleniumAddress: 'http://localhost:4444/wd/hub',
-		
+
 		specs: ['spec/dashboardSpec.js'],
 
 		jasmineNodeOpts: {defaultTimeoutInterval: 600000},
 		onPrepare: function(){
 			browser.manage().window().maximize();
-		} 
+			jasmine.getEnv().addReporter(
+					new Jasmine2HtmlReporter({
+						savePath: 'target/screenshots'
+					})
+			);
+		}
 };
