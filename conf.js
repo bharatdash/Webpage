6cb469bc03f6e5a3d53b793e313c9b22b36006fc
slathi <lathi.saumya@gmail.com>
2017-02-24 18:19:45 +0530

    Further changes and spell correction

--- conf.js
+++ conf.js
@@ -6,7 +6,7 @@ exports.config = {
 		//Specifies the framework we are using
 		framework: 'jasmine',
 		
-		//Address of the selenium-webserver (Although we dont reuire it here since we are using driect connect)
+		//Address of the selenium-webserver (Although we dont require it here since we are using direct connect)
 		seleniumAddress: 'http://localhost:4444/wd/hub',
 
 		//The spec files to be run
@@ -17,6 +17,8 @@ exports.config = {
 		
 		//Callback function before the specs are executed 
 		onPrepare: function(){
+			
+			//Maximises the window
 			browser.manage().window().maximize();
 			jasmine.getEnv().addReporter(
 					new Jasmine2HtmlReporter({
